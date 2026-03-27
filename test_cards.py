2bf8e19e3f6ff5789089f6687371367c9533e63e
theodora-6 <mjc10@kent.ac.uk>
2026-03-27 09:14:32 +0000

    added functions to test the name syntax filter thing and they both failed will try to fix now

--- test_cards.py
+++ test_cards.py
@@ -11,3 +11,15 @@ def test_get_card_not_none():
     card = get_card()
     assert card is not None
 
+def test_card_has_attributes():
+    card = get_card()
+    assert hasattr(card, "name")
+    assert hasattr(card, "rarity")
+
+def test_valid_name():
+    assert valid_name("Jinx")
+    assert valid_name("Vi")
+
+def test_invalid_name():
+    assert not valid_name("Jinx123")
+    assert not valid_name("123")
